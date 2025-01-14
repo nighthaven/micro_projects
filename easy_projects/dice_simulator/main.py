1f75c663b32fef70b8bbd217648d0e831d4b1045
Boris Le Bon <ooborislebonoo@gmail.com>
2025-01-14 11:08:54 +0100

    added dice simulator project

---
+++ easy_projects/dice_simulator/main.py
@@ -0,0 +1,41 @@
+import random
+
+
+def roll_dice(amount: int = 2) -> list[int]:
+    """
+    Rolls some dice and returns the results as a list.
+
+    :param amount: The amount of dice to roll.
+    :return: A list of dice rolls.
+    """
+    if amount <= 0:
+        raise ValueError
+
+    rolls: list[int] = []
+    for i in range(amount):
+        random_roll: int = random.randint(1, 6)
+        rolls.append(random_roll)
+
+    return rolls
+
+
+def main():
+    while True:
+        try:
+            user_input: str = input('How many dice would you like to roll? ')
+
+            # To exit the game
+            if user_input.lower() == 'exit':
+                print('Thanks for playing!')
+                break
+
+            # Try to parse the user_input to int
+            all_dice_result: list[int] = roll_dice(int(user_input))
+            print(*all_dice_result, sep=', ')
+            print(sum(all_dice_result))
+        except ValueError:
+            print('(Please enter a valid number)')
+
+
+if __name__ == '__main__':
+    main()
