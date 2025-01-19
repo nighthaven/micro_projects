43e36fbc99019faf4a94eec75f32368fb19042db
Boris Le Bon <ooborislebonoo@gmail.com>
2025-01-19 21:45:33 +0100

    email_sender

---
+++ bigger_projects/email_sender/credentials.py
@@ -0,0 +1,8 @@
+"""
+ATTENTION:
+Les informations du mail et mot de passe sont des informations sensible
+"""
+from typing import Final
+
+EMAIL: Final[str] = 'YOUR_EMAIL'
+PASSWORD: Final[str] = 'YOUR_PASSWORD'
